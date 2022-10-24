a1810283c189f94a3976bcea2e3c8f05575b26c0
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-10-24 09:36:30 +0200

    done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,57 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    //this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    var copy = [];
+    var n = cards.length;
+    var i;
+
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+      copy.push(array.splice(i, 1)[0]);
+    }
+
+    return copy;
+
+    /*var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;*/
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
+
+export default MemoryGame;
